a821b41c65e926bd4e038b0c6cb986bcabc700ce
Sinoe_Ruiz <zs23004727@gmail.com>
2025-04-10 10:16:35 -0600

    MYCV MEDIANAMENTE FUNCIONAL 0.0.1

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+// src/app/work-experience/work-experience.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { WorkExperienceService } from '../services/work-experience-service/work-experience.service';
+import { WorkExperience } from '../models/work-experience/work-experience.model';
+import { map } from 'rxjs/operators';
+import { DocumentChangeAction } from '@angular/fire/compat/firestore'; // Importación del tipo necesario
 
 @Component({
   selector: 'app-work-experience',
-  standalone: false,
   templateUrl: './work-experience.component.html',
-  styleUrl: './work-experience.component.css'
+  styleUrls: ['./work-experience.component.css']
 })
-export class WorkExperienceComponent {
+export class WorkExperienceComponent implements OnInit {
+
+  workExperienceList: WorkExperience[] = [];
+
+  constructor(public workExperienceService: WorkExperienceService) { }
+
+  ngOnInit(): void {
+    // Aquí se agrega el tipo adecuado para 'changes'
+    this.workExperienceService.getWorkExperience().snapshotChanges().pipe(
+      map((changes: DocumentChangeAction<any>[]) =>
+        changes.map((c: DocumentChangeAction<any>) => ({
+          id: c.payload.doc.id,
+          ...c.payload.doc.data()
+        }))
+      )
+    ).subscribe((data: WorkExperience[]) => {
+      this.workExperienceList = data;
+      console.log(this.workExperienceList);
+    });
+  }
 
 }
