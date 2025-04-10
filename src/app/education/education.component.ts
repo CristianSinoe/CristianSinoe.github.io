78405371274c5959302c73a279bd125a83e720c1
Sinoe_Ruiz <zs23004727@gmail.com>
2025-04-10 12:27:27 -0600

    MYCV MEDIANAMENTE FUNCIONAL 0.0.2

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EducationService } from '../services/education-service/education.service';
+import { Education } from '../models/education/education.model'; 
 
 @Component({
   selector: 'app-education',
-  standalone: false,
   templateUrl: './education.component.html',
-  styleUrl: './education.component.css'
+  styleUrls: ['./education.component.css']
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit {
+  
+  educationList: Education[] = [];
 
+  constructor(private educationService: EducationService) {}
+
+  ngOnInit(): void {
+    this.educationService.getEducation().subscribe(data => {
+      this.educationList = data;
+      console.log(this.educationList); // Verificar que los datos llegan correctamente
+    });
+  }
 }
--- src/app/services/education-service/education.service.ts
+++ src/app/services/education-service/education.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, DocumentChangeAction } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Education } from '../../models/education/education.model';
+import { map } from 'rxjs/operators';  // Importar 'map' correctamente
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducationService {
 
-  constructor() { }
+  private dbPath = '/Education';  // Ruta a la colección 'education'
+  educationRef: AngularFirestoreCollection<Education>;
+
+  constructor(private db: AngularFirestore) {
+    this.educationRef = db.collection(this.dbPath); // Inicializar la referencia de la colección
+  }
+
+  // Método que devuelve la colección de "education" desde Firestore
+  getEducation(): Observable<Education[]> {
+    return this.educationRef.snapshotChanges().pipe(
+      map((changes: DocumentChangeAction<any>[]) => // Especificar el tipo de 'changes'
+        changes.map((c: DocumentChangeAction<any>) => ({
+          id: c.payload.doc.id, 
+          ...c.payload.doc.data() as Education 
+        }))
+      )
+    );
+  }
 }
