d0ee8c8d0b37c0d1a7653862705c1a4c364b9816
Sinoe_Ruiz <zs23004727@gmail.com>
2025-04-10 05:01:53 -0600

    PROYECTO MEDIANAMENTE FUNCIONAL

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { EducationComponent } from './education/education.component';
+import { EducationService } from './services/education-service/education.service'; 
+import { SkillsComponent } from './skills/skills.component';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { InterestsComponent } from './interests/interests.component';
+
+import { AngularFireModule } from '@angular/fire/compat'; 
+import { environment } from '../environments/environment'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    CertificatesComponent,
+    LanguagesComponent,
+    InterestsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
